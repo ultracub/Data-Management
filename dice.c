381dcc040c05dcd83b5f1addb9ebbf4b6568e03f
ultracub <51847412+ultracub@users.noreply.github.com>
2019-07-11 20:58:02 +0900

    experimental
    experimental

--- dice.c
+++ dice.c
@@ -4,7 +4,12 @@
 int main(){
   
   int sai1,sai2,totalValue;  
+  char s [ 10 ];
   
+  ("What is your name?\n");
+  scanf("%s",s);
+  printf("Hello,%s!\n",s);
+
   printf("Rolling the dice...\n");
   sai1 = rand() % 5 + 1;
   sai2 = rand() % 5 + 1;
